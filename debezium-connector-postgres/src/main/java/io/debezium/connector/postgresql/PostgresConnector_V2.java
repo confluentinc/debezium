dd680c4c78885856570d5de0f356171d753c3514
rajdangwal <dangwalrajendra888@gmail.com>
2023-10-26 20:24:01 +0530

    Added wrapper PostgresConnector_V2

---
+++ debezium-connector-postgres/src/main/java/io/debezium/connector/postgresql/PostgresConnector_V2.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.debezium.connector.postgresql;
+
+/**
+ * A wrapper over PostgresConnector
+ * to allow multiple plugin versions support in CCloud
+ */
+public class PostgresConnector_V2 extends PostgresConnector {
+
+    public PostgresConnector_V2() {
+    }
+
+}
