e8f5c73fcd30dbd5ab7b14338f63a7af11c5d1c8
Sangeet Mishra <mail2sangeetmishra@gmail.com>
2024-10-23 22:23:23 +0530

    Add more trace level logs

--- debezium-core/src/main/java/io/debezium/pipeline/JmxUtils.java
+++ debezium-core/src/main/java/io/debezium/pipeline/JmxUtils.java
@@ -48,6 +48,7 @@ public class JmxUtils {
             for (int attempt = 1; attempt <= REGISTRATION_RETRIES; attempt++) {
                 try {
                     mBeanServer.registerMBean(mxBean, objectName);
+                    LOGGER.trace("Successfully registered MBean '{}'", objectName);
                     break;
                 }
                 catch (InstanceAlreadyExistsException e) {
@@ -92,6 +93,7 @@ public class JmxUtils {
             }
             try {
                 mBeanServer.unregisterMBean(objectName);
+                LOGGER.trace("Successfully unregistered MBean '{}'", objectName);
             }
             catch (InstanceNotFoundException e) {
                 LOGGER.info("Unable to unregister metrics MBean '{}' as it was not found", objectName);
@@ -110,7 +112,7 @@ public class JmxUtils {
             unregisterMXBean(objectName);
         }
         catch (MalformedObjectNameException e) {
-            LOGGER.info("Unable to unregister metrics MBean '{}' as it was not found", jmxObjectName);
+            LOGGER.info("Unable to unregister metrics MBean '{}' as it was not found '{}'", jmxObjectName, e.getMessage());
         }
     }
 
