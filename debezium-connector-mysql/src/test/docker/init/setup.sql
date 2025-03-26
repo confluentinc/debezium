7564d91140ab268647ef7702e1b03c82221064a7
Animesh Kumar <animkumar@confluent.io>
2025-03-26 09:44:20 +0530

    to be reverted. Done only to run with 8.0

--- debezium-connector-mysql/src/test/docker/init/setup.sql
+++ debezium-connector-mysql/src/test/docker/init/setup.sql
@@ -18,7 +18,7 @@ GRANT ALL PRIVILEGES ON *.* TO 'mysqluser'@'%';
 -- DATABASE:  emptydb
 -- ----------------------------------------------------------------------------------------------------------------
 CREATE DATABASE emptydb;
-RESET BINARY LOGS AND GTIDS; -- MySQL >= 8.4.x and 9.x
+RESET MASTER;
 CREATE DATABASE testing;
 CREATE TABLE testing.testing (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY);
 INSERT INTO testing.testing VALUES ();
