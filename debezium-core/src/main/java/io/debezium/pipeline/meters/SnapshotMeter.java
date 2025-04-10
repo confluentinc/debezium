9c5904e4c8f944294ba1ac4a878b6016d26e526c
Sangeet Mishra <mail2sangeetmishra@gmail.com>
2025-04-10 12:03:49 +0530

    Revert "fix checkstyle failures"
    This reverts commit 2138c5ae6930e60f868450da4a8f84275084c418.

--- debezium-core/src/main/java/io/debezium/pipeline/meters/SnapshotMeter.java
+++ debezium-core/src/main/java/io/debezium/pipeline/meters/SnapshotMeter.java
@@ -11,6 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
 
