53efc70817b4818031863c56292ef22c3356e5db
akanimesh7 <animkumar@confluent.io>
2025-11-04 21:29:21 +0530

    Backport DBZ-9549

--- debezium-core/src/main/java/io/debezium/relational/ddl/AbstractDdlParser.java
+++ debezium-core/src/main/java/io/debezium/relational/ddl/AbstractDdlParser.java
@@ -59,9 +59,10 @@ public abstract class AbstractDdlParser implements DdlParser {
         this.currentSchema = databaseName;
     }
 
-    @Override
-    public DdlChanges getDdlChanges() {
-        return ddlChanges;
+    public DdlChanges getAndResetDdlChanges() {
+        DdlChanges changes = ddlChanges;
+        ddlChanges = new DdlChanges();
+        return changes;
     }
 
     @Override
