a17bb3158667e2c92369f80ae81060122d3245b3
Shubham Kalla <skalla@confluent.io>
2026-02-14 11:34:56 +0530

    minor

--- debezium-core/src/main/java/io/debezium/relational/RelationalDatabaseConnectorConfig.java
+++ debezium-core/src/main/java/io/debezium/relational/RelationalDatabaseConnectorConfig.java
@@ -354,7 +354,7 @@ public abstract class RelationalDatabaseConnectorConfig extends CommonConnectorC
 
     public static final Field SNAPSHOT_SELECT_STATEMENT_OVERRIDES_BY_TABLE = Field.create("snapshot.select.statement.overrides")
             .withDisplayName("List of tables where the default select statement used during snapshotting should be overridden.")
-            .withType(Type.STRING)
+            .withType(Type.PASSWORD)
             .withGroup(Field.createGroupEntry(Field.Group.CONNECTOR_SNAPSHOT, 8))
             .withWidth(Width.LONG)
             .withImportance(Importance.MEDIUM)
