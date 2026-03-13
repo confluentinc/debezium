e1b32e52267f965fc7ef09f6009ae30699392910
Bhagyashree <bhagyashree@confluent.io>
2026-03-13 13:41:59 +0530

    Add cherry-pick config for debezium sync

---
+++ .cherry-picks/env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export CHERRY_PICK_BASE_BRANCH=worktree-run_2
+export CHERRY_PICK_BASE_REMOTE=origin
+export CHERRY_PICK_UPSTREAM_URL=https://github.com/confluentinc/debezium
+export CHERRY_PICK_UPSTREAM_REMOTE=origin
+export CHERRY_PICK_CONFIG_REF=HEAD
+export CHERRY_PICK_GH_REPO=confluentinc/debezium
+export CHERRY_PICK_DRY_RUN=true
+# Upstream tag the target branch is based on (for skip detection)
+export TARGET_UPSTREAM_TAG=v3.3.2.Final
