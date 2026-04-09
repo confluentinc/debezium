8b52aa3379343a342a61302b4eae7a48b232f4cd
KGaneshDatta <kdatta@confluent.io>
2026-04-09 17:08:27 +0530

    Skip Flaky Tests in MySQL and Sql Server Connectors

--- debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/BlockingSnapshotIT.java
+++ debezium-connector-mysql/src/test/java/io/debezium/connector/mysql/BlockingSnapshotIT.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.jetbrains.annotations.NotNull;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 
 import io.debezium.config.CommonConnectorConfig;
 import io.debezium.config.Configuration;
@@ -172,6 +173,12 @@ public class BlockingSnapshotIT extends AbstractBlockingSnapshotTest {
 
         assertThat(schemaChangesDdls.get(schemaChangesDdls.size() - 1)).isEqualTo(getDdlString(databaseVersionResolver));
 
+    }
+
+    @Override
+    @Ignore
+    public void readsSchemaOnlyForSignaledTables() throws Exception {
+        super.readsSchemaOnlyForSignaledTables();
     }
 
     @NotNull
