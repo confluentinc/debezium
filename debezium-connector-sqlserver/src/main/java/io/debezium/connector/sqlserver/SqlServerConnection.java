5cefdd49a48f254d09078ba9feb6efd385c2701b
Bhagyashree <bhagyashree@confluent.io>
2026-07-31 13:27:40 +0530

    debezium/dbz#2012 Document event_serial_no details as per query mode and updated integration test assertion with event_serial_no
    Cherry-Picked-From: 92f20ba557a60b181643e7fec7b59a349c4ed928
    Cherry-Picked-By: Bhagyashree <bhagyashree@confluent.io>
    Conflicts: sqlserver.adoc
    
    Signed-off-by: Bhagyashree <bhagyashree@confluent.io>

--- debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerConnection.java
+++ debezium-connector-sqlserver/src/main/java/io/debezium/connector/sqlserver/SqlServerConnection.java
@@ -182,8 +182,8 @@ public class SqlServerConnection extends JdbcConnection {
         this.optionRecompile = optionRecompile;
     }
 
-    static String buildGetAllChangesForTableQuery(SqlServerConnectorConfig.DataQueryMode dataQueryMode,
-                                                  Set<Envelope.Operation> skippedOperations) {
+    private String buildGetAllChangesForTableQuery(SqlServerConnectorConfig.DataQueryMode dataQueryMode,
+                                                   Set<Envelope.Operation> skippedOperations) {
         boolean isDirectMode = dataQueryMode == SqlServerConnectorConfig.DataQueryMode.DIRECT;
         String result = "";
         List<String> where = new LinkedList<>();
@@ -254,7 +254,7 @@ public class SqlServerConnection extends JdbcConnection {
         return result;
     }
 
-    private static boolean hasSkippedOperations(Set<Envelope.Operation> skippedOperations) {
+    private boolean hasSkippedOperations(Set<Envelope.Operation> skippedOperations) {
         if (!skippedOperations.isEmpty()) {
             for (Envelope.Operation operation : skippedOperations) {
                 switch (operation) {
